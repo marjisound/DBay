f2efbca79e69f4f23e4695a48a9d375c8489b269
marjisound <Marjan@dhcp-37-235.xlate.eduroam.ucl.ac.uk>
2016-02-26 17:01:40 +0000

    createAuction

--- js/main.js
+++ js/main.js
@@ -1,15 +1,52 @@
-$('#postcode_lookup').getAddress({
-    api_key: 'YOUR_API_KEY', 
-    output_fields:{
-        line_1: '#line1',
-        line_2: '#line2',
-        line_3: '#line3',
-        post_town: '#town',
-        county: '#county',
-        postcode: '#postcode'
-    },
-<!--  Optionally register callbacks at specific stages -->                                                                                                               
-    onLookupSuccess: function(data){/* Your custom code */},
-    onLookupError: function(){/* Your custom code */},
-    onAddressSelected: function(elem,index){/* Your custom code */}
+   // var cp_obj = CraftyPostcodeCreate();
+   // cp_obj.set("access_token", "xxxxx-xxxxx-xxxxx-xxxxx"); // your token here
+   // cp_obj.set("result_elem_id", "crafty_postcode_result_display");
+   // cp_obj.set("form", "address");
+   // cp_obj.set("elem_company"  , "companyname");
+   // cp_obj.set("elem_street1"  , "address1");
+   // cp_obj.set("elem_street2"  , "address2");
+   // cp_obj.set("elem_street3"  , "address3");
+   // cp_obj.set("elem_town"     , "town");
+   // cp_obj.set("elem_postcode" , "postcode");
+
+$(document).ready(function(){
+  $("#btnFindPostCode").click(function(){
+    $("#spnPostcode_result_display").show();
+    $("#cmbPostCode").hide();
+    $("#spnAJAXWait").show();
+    var vPostCode = $("#postcode").val();
+    vPostCode = vPostCode.replace(' ', '');
+    var getAddressAPI = "/marjan.com/include/getaddress.php?postcode="+vPostCode;
+    $.getJSON( getAddressAPI, {
+      format: "json"
+    })
+      .done(function( data ) {
+        var optionAddress = '';
+        if(data != null){
+          for(iCnt = 0, cnt = data.length; iCnt < cnt; iCnt++){
+            optionAddress += '<option value="'+data[iCnt]+'">'+data[iCnt]+'</optioon>';
+          }
+          $("#spnAJAXWait").hide();
+          $("#cmbPostCode").html(optionAddress);
+          $("#cmbPostCode").show();
+        }
+        else{
+          $("#spnAJAXWait").hide();
+          $("#cmbPostCode").html('');
+          $("#spnPostcode_result_display").hide(); 
+          alert('Your postcode is not valid.\nPlease enter a valid postcode.'); 
+          $("#address1").val('');
+          $("#address2").val('');
+          $("#town").val('');    
+        }
+      });
+   });
+
+  $("#cmbPostCode").change(function(){
+    var arrAddress = $(this).val().split(',');
+    $("#address1").val(arrAddress[0]);
+    $("#address2").val(arrAddress[4]);
+    $("#town").val(arrAddress[5]);
+
+  });
 });
