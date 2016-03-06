fd20a55c9d6913634e79155f7324bf8355e43186
tevans1000 <tevans1000@googlemail.com>
2016-03-06 13:30:48 +0000

    Update place-bid.php

--- place-bid.php
+++ place-bid.php
@@ -1,9 +1,6 @@
 <?php
 
-// Server script called from auction page to place a bid on an item
-
 // Set database connection, user id, auction id
-// TODO: check existence of all variables
 $dbConnection = $_SESSION["dbConnection"];
 $userID     = $_SESSION["userID"];
 $auctionID  = $_SESSION["currAuctionID"];
@@ -11,40 +8,53 @@ $auctionID  = $_SESSION["currAuctionID"];
 // Retrieve user's bid from form
 $userBid = $_POST["amount"];
 
-// Set, prepare bind and execute SQL to find auction end date
-// (Assuming PDO)
-$stmt = $connection -> prepare("SELECT `end_date`
+// Set, prepare bind and execute SQL to find auction end date & start price
+// (Assuming mysqli)
+// Optional: also fetch seller id if sellers restricted from bidding in own auctions
+$stmt = $dbConnection -> prepare("SELECT `end_date`, `start_price`
                                 FROM `auction`
-                                WHERE `auction_id` = :auction
+                                WHERE `auction_id` = ?
                                ");
-$stmt -> bind_param(':auction', $auctionID);
-$dbConnection -> execute($stmt);
-// TODO: Error handling if auction not found
-$endDate = $dbConnection -> fetchColumn();
+$stmt -> bind_param("i", $auctionID);
+$stmt -> execute();
+// TODO: Exception handling for missing auction / duplicate auctions
+$stmt -> bind_result($endDate, $startPrice);
+$stmt -> fetch();
+
+// Optional stop & return to auction page if seller id = buyer id
+/*
+if ( $userID == $sellerID ){
+$_SESSION["bidStatus"] = array("auction" => $auctionID, "status" => "RESTRICTED", "when" => date("Y-m-d H:i:s"));
+header("Location: auction.php?auction-id=$auctionID");
+}
+*/
 
 // Set, prepare bind and execute SQL to find current high bid
-// (Assuming PDO)
-$stmt = $connection -> prepare("SELECT MAX(`amount`)
+// (Assuming mysqli)
+$stmt = $dbConnection -> prepare("SELECT MAX(`amount`)
                                 FROM `bid`
-                                WHERE `auction_id` = :auction
+                                WHERE `auction_id` = ?
                                 GROUP BY `auction_id`
                                ");
-$stmt -> bind_param(':auction', $auctionID);
+$stmt -> bind_param("i", $auctionID);
 $dbConnection -> execute($stmt);
-// TODO: Checking for first bid placed (i.e. $maxbid does not exist)
-$maxBid = $dbConnection -> fetchColumn();
+$stmt -> bind_result($maxBid);
+if ($stmt -> fetch){
+	// A previous bid exists
+} else {
+	$maxbid = $startPrice;
+}
+
 
 // Enter bid or fail according to bid value (and date)
-
 if ($userBid > $maxBid){
     if(date("Y-m-d H:i:s") < $endDate){
-        $stmt = $connection -> prepare("INSERT INTO `bid`
-                                       VALUES (:user, :auction, :amount, NOW())
-                                       ");
-        $stmt -> bind_param(':user', $userID);
-        $stmt -> bind_param(':auction', $auctionID);
-        $stmt -> bind_param(':amount', $userBid);
+        $stmt = $dbConnection -> prepare("INSERT INTO `bid`
+                                       VALUES (?, ?, ?, NOW())
+                                       ");//   user, auction, amount 
+        $stmt -> bind_param("iid", $userID, $auctionID, $userBid);
         $dbConnection -> execute($stmt);
+        // TODO: error handling for insertion failure
         $_SESSION["bidStatus"] = array("auction" => $auctionID, "status" => "SUCCESS", "when" => date("Y-m-d H:i:s"));
         header("Location: auction.php?auction-id=$auctionID");
     } else {
@@ -55,5 +65,6 @@ if ($userBid > $maxBid){
     $_SESSION["bidStatus"] = array("auction" => $auctionID, "status" => "TOO LOW", "when" => date("Y-m-d H:i:s"));
     header("Location: auction.php?auction-id=$auctionID");
 }
+// nb: header could redirect to seperate bid confirmation page
 
 ?>
