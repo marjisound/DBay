421bd241eaabbc5c1986a7e1c689a60c1033d2c3
marjisound <Marjan@dhcp-75-116.xlate.eduroam.ucl.ac.uk>
2016-03-15 12:55:06 +0000

    11

--- logout.php
+++ logout.php
@@ -1,8 +1,11 @@
-<?php include 'C:/wamp/www/Db_project/includes/sessions.php' ?>
-<?php require_once'C:/wamp/www/Db_project/includes/functions.php' ?>
+<?php include 'include/sessions.php' ?>
+<?php require_once'include/functions.php' ?>
 
 <?php
-		$_SESSION['user_id'] = null;
+
+		session_unset();
+		session_destroy();
+		session_write_close();
 		echo "You have logged out"."<br/>";
 		echo "<a href='index.php'>click here</a>"."<br/>";
 		echo "To go back to home page";
